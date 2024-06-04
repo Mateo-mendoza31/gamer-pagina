a458d028734af1cf7c965ab541f2e7ad98c8e6f3
mateo mendoza <mateomendoza310107gmail.com>
2024-06-03 21:11:11 -0300

    primer commit

---
+++ javascrip.js
@@ -0,0 +1,8 @@
+let name ="mateo";
+let years ="17";
+console.log(years + " " + name);
+
+alert("Bienvenido a mi pagina");
+document.getElementById("Ingresaar").innerHTML = "¡Bienvenido a mi página!";
+console.log("¡Bienvenido a mi página!");
+
---
+++ paginas/java2.js
@@ -0,0 +1,6 @@
+
+alert("Bienvenido a Trailer")
+
+let nombre="Mateo";
+let years="17";
+console.log(nombre + " " + years);
